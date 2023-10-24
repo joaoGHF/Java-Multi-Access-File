8e5d7a45e92d9c990921558f481275999afe74c4
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-10-24 20:43:49 -0300

    Update ou Create methods in FileController

--- MultiAccessFile/src/org/multi_access_file/FileController.java
+++ MultiAccessFile/src/org/multi_access_file/FileController.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class FileController {
 	public static void initFile(File file) {
@@ -29,9 +30,51 @@ public class FileController {
 	public static synchronized void writeLine(String line, File staticFile) {
 		synchronized (staticFile) {
 			if (staticFile.canWrite()) {
-				try (BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(staticFile)))) {
+				//try (BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(staticFile)))) {
+				try (BufferedWriter bf = new BufferedWriter(new FileWriter(staticFile, true))) {
 					bf.write(line);
-					System.out.println("Writed in file " + staticFile.getName() + " the content: \n" + line);
+					bf.newLine();
+					System.out.println("Added in file " + staticFile.getName() + " the content: \n" + line);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				System.out.println("Not can write in file: " + staticFile.getName() + " in path "
+						+ staticFile.getPath());
+			}
+		}
+	}
+
+	public static synchronized ArrayList<String> readLines(File staticFile) {
+		synchronized (staticFile) {
+			if (staticFile.canRead()) {
+				try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(staticFile)))) {
+					ArrayList<String> lines = new ArrayList<>();
+					while (br.ready()) {
+						lines.add(br.readLine());
+					}
+
+					return lines;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("Not can read from the file: " + staticFile.getName() + " in path "
+						+ staticFile.getPath());
+			}
+
+			return null;
+		}
+	}
+
+	public static void clearFile(File staticFile) {
+		synchronized (staticFile) {
+			if (staticFile.canWrite()) {
+				try (BufferedWriter bf = new BufferedWriter(new FileWriter(staticFile, false))) {
+					bf.write("");
+					System.out.println("Cleared the file " + staticFile.getName());
 
 				} catch (IOException e) {
 					e.printStackTrace();
