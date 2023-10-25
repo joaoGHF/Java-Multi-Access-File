cd6a647f4ba08f396fcac90887d3088abdf1888a
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-10-24 21:37:37 -0300

    Remove unused imports

--- MultiAccessFile/src/org/multi_access_file/FileController.java
+++ MultiAccessFile/src/org/multi_access_file/FileController.java
@@ -4,11 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class FileController {
