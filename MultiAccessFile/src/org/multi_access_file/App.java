f4ec96d2f8127aeae96fda38397e738538398067
joaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2023-10-25 21:40:35 -0300

    Adding docs

--- MultiAccessFile/src/org/multi_access_file/App.java
+++ MultiAccessFile/src/org/multi_access_file/App.java
@@ -3,8 +3,47 @@ package org.multi_access_file;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * <p>
+ * The class {@code App} is the main class of this project. The
+ * {@code MultiAccessFile} program create (if not created) a file with a
+ * {@code File} object that will receive the pathname {@code "arq.txt"} and is
+ * located in the root path of the project, next will initialize and clear the
+ * file using the statics methods: {@code FileController.initFile(File)} and
+ * {@code FileController.clearFile(File)}. Will store in a
+ * {@code ArrayList<Thread>} a amount of {@code WriteThread} objects and finally
+ * start each thread in a for each loop.
+ * </p>
+ * 
+ * @see org.multi_access_file.FileController
+ * @see org.multi_access_file.WriteThread
+ * @see java.util.ArrayList
+ * @see java.lang.Thread
+ */
 public class App {
+	/**
+	 * <p>
+	 * Defines the number of threds {@code WriteThread} that will be created in the
+	 * execution of the main method.
+	 * </p>
+	 * 
+	 * @see org.multi_access_file.WriteThread
+	 */
 	private static final int NUMBER_THREADS = 100;
+
+	/**
+	 * <p>
+	 * The main method of the program. Responsible for create, initialize and clear
+	 * the file; also responsible for create, initialize and start all subsequent
+	 * threads.
+	 * </p>
+	 * 
+	 * @param args are dont used in this program
+	 * 
+	 * @see org.multi_access_file.FileController#initFile(File)
+	 * @see org.multi_access_file.FileController#clearFile(File)
+	 * @see org.multi_access_file.WriteThread
+	 */
 	public static void main(String[] args) {
 		File file = new File("arq.txt");
 
--- MultiAccessFile/src/org/multi_access_file/FileController.java
+++ MultiAccessFile/src/org/multi_access_file/FileController.java
@@ -5,7 +5,26 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileController {
+/**
+ * <p>
+ * The {@code FileController} class is a abstract class, with all methods
+ * static. {@code FileController} have the methods {@code initFile(File)},
+ * {@code writeLine(String, File)} and {@code clearFile(File)}. The last two
+ * mentioned methods are synchronizeds, but the first isn't because they are
+ * invoked only in the init of the program flow and have no risk of corrupting.
+ * </p>
+ * 
+ * @see org.multi_access_file.FileController#initFile(File)
+ * @see org.multi_access_file.FileController#writeLine(String, File)
+ * @see org.multi_access_file.FileController#clearFile(File)
+ */
+public abstract class FileController {
+	// TODO: continue here
+	/** <p>
+	 * 
+	 * </p>
+	 * @param file is a {@code File} object, must be an file, not a directory.
+	 */
 	public static void initFile(File file) {
 		if (file.exists()) {
 			System.out.println("Arquivo existente...");
